c70822de5331b0d96893fac6ec5a0ee716f6f63d
EduardoRibero <ribeirodomingues2020@gmail.com>
2023-11-29 19:12:15 -0300

    elif

---
+++ elif.py
@@ -0,0 +1,12 @@
+home = "Thailandia"
+
+if home == "PAris":
+    print("Hello Paris")
+elif home == "Germania":
+    print("Hello Germania")
+elif home == "Barcelona":
+    print("Hello Barcelona")
+elif home == "Thailandia":
+    print("Hello Thailandia")
+else:
+    print("Hello World")
