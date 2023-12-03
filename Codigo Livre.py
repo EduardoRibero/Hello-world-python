85bf411b7ce34fd69ee92913d4b3d737c22b88f4
EduardoRibero <ribeirodomingues2020@gmail.com>
2023-12-03 09:35:00 -0300

    Codigo Livre

---
+++ Codigo Livre.py
@@ -0,0 +1,14 @@
+#Codio livre
+
+value = 777
+
+senha = input("Qual a senha?")
+
+if senha == value:
+    print("Beleza! Seja vem bem vindo então.")
+    var = input("Fale um nuemero:")
+    var0 = input("Fale outro nuemero:")
+    try:
+        var / var0
+        exption ZeroDivisionError
+    
---
+++ Containes-listas.py
@@ -0,0 +1,24 @@
+# Listas são containers que guardam dados (Objetos) em ordem.
+
+fruit = list(); #Uma forma de criar um lista fazia
+fruit.append("Primeio Item da Lista") # Inserindo dados em uma lista
+
+
+print(fruit)
+
+fruit.append("Segundo Item da lista")
+
+print(fruit)
+
+fruit2 = [] #Outra forma de criar uma lista
+
+fruit2.append("Item da lista")
+
+print(fruit2)
+
+fruit3 = ["Valor1", "Valor2", "Valor3"]#Criando já com dados
+print(fruit3)
+
+
+
+
