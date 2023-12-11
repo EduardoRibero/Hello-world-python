a0a5e71c214c035497bcd409a35f21c90bd48619
EduardoRibero <ribeirodomingues2020@gmail.com>
2023-12-11 13:47:00 -0300

    Dicionarios

---
+++ Conteiners.py
@@ -0,0 +1,20 @@
+dicionario = {
+    "location":(40.454, 50.354),
+    "celebs":["froid","bob","quick"],
+    "facts":{
+        "state":"NY",
+        "country":"America"
+        }
+    }
+
+print(dicionario)
+
+
+meu_dicionario = {
+                    "nome": "Eduardo Ribeiro",
+                    "idade": 22,
+                    "online": True,
+                    "habilidade": ["java", "SQL", "git"]
+                }
+
+print(meu_dicionario)
